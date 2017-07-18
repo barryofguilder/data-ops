b7da4d92273eeafa24dd1d0317b8d5555ba28369
Jason Barry <jbarry@nrchealth.com>
2017-07-18 09:25:26 -0400

    Move channel panels around and add channel state

---
+++ app/pods/components/channel-state/component.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import CHANNEL_STATE from 'data-ops/utils/channel-constants';
+
+export default Ember.Component.extend({
+  tagName: 'span',
+  classNames: ['badge'],
+  classNameBindings: ['stateClass'],
+
+  state: null,
+  stateClass: Ember.computed('state', function() {
+    let state = this.get('state');
+
+    if (state === CHANNEL_STATE.ACTIVE) {
+      return 'badge-success';
+    } else if (state === CHANNEL_STATE.INACTIVE) {
+      return 'badge-danger';
+    } else if (state === CHANNEL_STATE.DEBUG) {
+      return 'badge-warning';
+    }
+  })
+});
--- mirage/factories/channel.js
+++ mirage/factories/channel.js
@@ -1,7 +1,20 @@
 import { Factory } from 'ember-cli-mirage';
+import CHANNEL_STATE from 'data-ops/utils/channel-constants';
+
+const STATES = [
+  CHANNEL_STATE.ACTIVE,
+  CHANNEL_STATE.ACTIVE,
+  CHANNEL_STATE.INACTIVE,
+  CHANNEL_STATE.DEBUG,
+  CHANNEL_STATE.ACTIVE,
+  CHANNEL_STATE.INACTIVE,
+];
 
 export default Factory.extend({
   name(i) {
     return `Channel ${i+1}`;
+  },
+  state(i) {
+    return STATES[i];
   }
 });
